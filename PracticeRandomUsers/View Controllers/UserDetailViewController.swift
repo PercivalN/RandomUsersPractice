51b5d3d0cf2217f9aaf2962426ae0fb2c94e1aaf
Percy <percival.ngan@gmail.com>
2019-11-01 16:36:11 -0500

    Added storyboard and view controllers

---
+++ PracticeRandomUsers/View Controllers/UserDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  UserDetailViewController.swift
+//  PracticeRandomUsers
+//
+//  Created by Percy Ngan on 11/1/19.
+//  Copyright © 2019 Lamdba School. All rights reserved.
+//
+
+import UIKit
+
+class UserDetailViewController: UIViewController {
+
+	@IBOutlet weak var userImageView: UIImageView!
+	@IBOutlet weak var nameLabel: UILabel!
+	@IBOutlet weak var phoneNumberLabel: UILabel!
+	@IBOutlet weak var emailLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
